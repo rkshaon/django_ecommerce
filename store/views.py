45ecaa42bff7e8ae4e942eaf640b6514f43072f8
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-11-25 02:24:13 +0600

    template done

---
+++ store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def store(request):
+    context = {}
+    return render(request, 'store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'checkout.html', context)

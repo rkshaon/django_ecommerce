ad8d255a40c9e8f0f7756dc21667a36d2f406cc8
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-11-25 16:08:28 +0600

    models done

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from store.models import Customer, Product, Order, OrderItem, ShippingAddress
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from store.models import *
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {
+        'products': products,
+    }
     return render(request, 'store.html', context)
 
 def cart(request):

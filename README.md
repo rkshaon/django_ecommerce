c6b8e786aa6991f437839d76744c887e98e627f1
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-11-25 18:31:34 +0600

    cart and checkout for authenticate customer

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 1. Install python
 2. Open Command Prompt
 3. Write `git clone https://github.com/rkshaon/django_ecommerce.git` and press enter
-3. Write `pip install -r requirements.txt` and press enter
-4. Write `python manage.py runsever` and enter
+4. Write `pip install -r requirements.txt` and press enter
+5. Write `python manage.py runsever` and press enter
+6. Open the browser and put `127.0.0.1:800` on address bar and press enter
 
 
 ## Features:
--- store/views.py
+++ store/views.py
@@ -9,9 +9,29 @@ def store(request):
     return render(request, 'store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'checkout.html', context)
